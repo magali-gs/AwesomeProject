d39f661999ab68031614c0e6aa5c5656ea84640e
Magali Gonçalves <magali.goncalvess@gmail.com>
2022-09-03 15:52:33 +0200

    Add Details screens

---
+++ android/app/src/screens/DetailsScreen.js
@@ -0,0 +1,13 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const DetailsScreen = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+};
+
+export default DetailsScreen;
