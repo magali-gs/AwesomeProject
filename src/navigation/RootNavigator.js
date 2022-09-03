a3ea2e7d9b135cb968e7944c0ae5f66b66f04681
Magali Gonçalves <magali.goncalvess@gmail.com>
2022-09-03 16:23:14 +0200

    Configure Deeo Linking and URI schemes

--- src/navigation/RootNavigator.js
+++ src/navigation/RootNavigator.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { ActivityIndicator } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -7,9 +8,13 @@ import DetailsScreen from '../screens/DetailsScreen';
 
 const RootStack = createNativeStackNavigator();
 
+const linking = {
+  prefixes: ['awesomeProject://'],
+};
+
 const RootNavigator = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={linking}>
       <RootStack.Navigator>
         <RootStack.Screen name="Home" component={HomeScreen} />
         <RootStack.Screen name="Details" component={DetailsScreen} />
